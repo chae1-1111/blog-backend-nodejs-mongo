7c0f1245332783c982a13208106cfb15e685e344
chae1-1111 <dla4212@naver.com>
2022-01-10 00:31:25 +0900

    finalTest

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ testRouter.route("/").get((req, res) => {
     res.writeHead("200", { "content-type": "text/html;charset=utf8" });
     res.write("<h2 style='text-align: center'>테스트 페이지 입니다.</h2>");
     res.write("<p style='text-align: center'>찾아와주셔서 감사합니다.</p>");
+    res.write("<p style='text-align: center'>Hello</p>");
     res.end();
 });
 testRouter.use(cors());
