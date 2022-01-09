377e496af89568b697a28d995258cb308b770b5d
Ubuntu <ubuntu@ip-172-31-38-41.ap-northeast-2.compute.internal>
2022-01-09 19:31:01 +0900

    Setting in AWS

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
+const cors = require("cors");
 
 const app = express();
 const port = 3000;
@@ -15,6 +16,16 @@ app.use("/member", memberRouter);
 // 포스트 라우터
 const boardRouter = require("./router/board");
 app.use("/board", boardRouter);
+
+const testRouter = express.Router();
+testRouter.route("/").get((req, res) => {
+	res.writeHead("200", {"content-type":"text/html;charset=utf8"});
+	res.write("<h2 style='text-align: center'>테스트 페이지</h2>");
+	res.end();
+});
+testRouter.use(cors());
+
+app.use("/test",testRouter);
 
 app.listen(port, () => {
     console.log(`${port}포트 서버 실행중...`);
